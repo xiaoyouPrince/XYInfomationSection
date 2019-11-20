42bdd2219e8378c8b6af1e75952defe651dcb4cb
xiaoyouPrince <xiaoyouPrince@163.com>
2019-11-20 21:55:22 +0800

    添加一个综合应用实例

--- SectionDemo/Controller/section2/FamilyMember/FamilyMemberListViewController.m
+++ SectionDemo/Controller/section2/FamilyMember/FamilyMemberListViewController.m
@@ -10,23 +10,115 @@
 
 @interface FamilyMemberListViewController ()
 
+/** section 信息输入项 */
+@property (nonatomic, strong)       XYInfomationSection *section;
+/** 确认按钮 */
+@property (nonatomic, strong)       UIButton *saveBtn;
+
 @end
 
 @implementation FamilyMemberListViewController
 
+#pragma mark - lazyload
+
+- (XYInfomationSection *)section
+{
+    if (!_section) {
+        XYInfomationSection *section = [XYInfomationSection new];
+        _section = section;
+        
+        XYInfomationItem *item1 = [XYInfomationItem modelWithTitle:@"姓名" titleKey:@"memberName" type:0 value:nil placeholderValue:nil disableUserAction:NO];
+        XYInfomationItem *item2 = [XYInfomationItem modelWithTitle:@"用户性别" titleKey:@"memberSex" type:1 value:nil placeholderValue:nil disableUserAction:NO];
+        XYInfomationItem *item3 = [XYInfomationItem modelWithTitle:@"证件类型" titleKey:@"memberCardType" type:1 value:nil placeholderValue:nil disableUserAction:NO];
+        XYInfomationItem *item4 = [XYInfomationItem modelWithTitle:@"证件号码" titleKey:@"memberIdCardNo" type:0 value:nil placeholderValue:nil disableUserAction:NO];
+        XYInfomationItem *item5 = [XYInfomationItem modelWithTitle:@"出生日期" titleKey:@"memberBirthDate" type:1 value:nil placeholderValue:nil disableUserAction:NO];
+        XYInfomationItem *item6 = [XYInfomationItem modelWithTitle:@"与本人关系" titleKey:@"relationShip" type:1 value:nil placeholderValue:nil disableUserAction:NO];
+        
+        section.dataArray = @[item1,item2,item3,item4,item5,item6];
+        __weak typeof(self) weakSelf = self;
+        section.cellClickBlock = ^(NSInteger index, XYInfomationCell * _Nonnull cell) {
+            [weakSelf sectionCellClicked:cell];
+        };
+    }
+    return _section;
+}
+
+- (UIButton *)saveBtn
+{
+    if (!_saveBtn) {
+        UIButton *saveBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [self.scrollView addSubview:saveBtn];
+        saveBtn.frame = CGRectMake(0, 0, 300, 40);
+        _saveBtn = saveBtn;
+        
+        // 设置保存按钮背景渐变色
+        CAGradientLayer *gradientLayer =  [CAGradientLayer layer];
+        gradientLayer.startPoint = CGPointMake(0, 0);
+        gradientLayer.endPoint = CGPointMake(1, 0);
+        gradientLayer.locations = @[@(0.5),@(1.0)];//渐变点
+        NSArray *colors = @[(id)[HEXCOLOR(0xf44b42) CGColor],(id)[HEXCOLOR(0xd8261d) CGColor]];
+        [gradientLayer setColors:colors];//渐变数组
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            gradientLayer.frame = saveBtn.bounds;
+            [saveBtn.layer insertSublayer:gradientLayer atIndex:0];
+            saveBtn.layer.cornerRadius = 10;
+            saveBtn.clipsToBounds = YES;
+            [saveBtn setTitle:@"保存" forState:UIControlStateNormal];
+        });
+        
+        [saveBtn addTarget:self action:@selector(saveBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _saveBtn;
+}
+
+
+#pragma mark - life circle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = HEXCOLOR(0xf6f6f6);
+    
+    /*
+     结构：上面输入内容 + 下面确定按钮
+     */
+    
+    // 1.添加内容组
+    [self setHeaderView:self.section edgeInsets:UIEdgeInsetsMake(20, 15, 30, 15)];
+
+    // 2. 确定按钮
+    [self setFooterView:self.saveBtn edgeInsets:UIEdgeInsetsMake(0, 50, 50, 50)];
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - actions
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)saveBtnClick:(id)sender{
+    
+    [SVProgressHUD showInfoWithStatus:[NSString stringWithFormat:@"所选内容:\n %@",_section.contentKeyValues.description]];
+    
 }
-*/
+
+- (void)sectionCellClicked:(XYInfomationCell *)cell{
+    
+    if (cell.model.type == XYInfoCellTypeInput) {
+        // 这里控制键盘弹出，能正常展示到 输入框的下面
+        if (cell.model.disableUserAction) {
+            [SVProgressHUD showSuccessWithStatus:@"此cell仅用于展示"];
+        }else
+        {
+            [SVProgressHUD showSuccessWithStatus:@"弹出键盘，输入内容"];
+        }
+    }
+    
+    if (cell.model.type == XYInfoCellTypeChoose) {
+        // 这里控制选择类型的cell,根据cell.model.titleKey去加载要展示的正确数据
+        if (cell.model.disableUserAction) {
+            [SVProgressHUD showSuccessWithStatus:@"此cell仅用于展示"];
+        }else
+        {
+            [SVProgressHUD showSuccessWithStatus:@"弹出pickerView，选择对应项目值"];
+        }
+    }
+}
 
 @end
