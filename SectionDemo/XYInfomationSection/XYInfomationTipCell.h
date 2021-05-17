78a0c4ec775f1e718671d438f2848466bc899ccb
xiaoyouPrince <xiaoyouPrince@163.com>
2021-05-17 22:14:28 +0800

    提供新 tipCell

---
+++ SectionDemo/XYInfomationSection/XYInfomationTipCell.h
@@ -0,0 +1,17 @@
+//
+//  XYInfomationTipCell.h
+//  SectionDemo
+//
+//  Created by 渠晓友 on 2021/5/17.
+//  Copyright © 2021 渠晓友. All rights reserved.
+//
+
+#import "XYInfomationCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XYInfomationTipCell : XYInfomationCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SectionDemo/XYInfomationSection/XYInfomationTipCell.m
@@ -0,0 +1,41 @@
+//
+//  XYInfomationTipCell.m
+//  SectionDemo
+//
+//  Created by 渠晓友 on 2021/5/17.
+//  Copyright © 2021 渠晓友. All rights reserved.
+//
+
+#import "XYInfomationTipCell.h"
+
+@implementation XYInfomationTipCell
+{
+    UILabel *_titleLabel;
+}
+
+- (instancetype)init{
+    self = [super initWithFrame:CGRectZero];
+    
+    if (self) {
+        _titleLabel = [UILabel new];
+        _titleLabel.textAlignment = NSTextAlignmentLeft;
+        _titleLabel.textColor = UIColor.lightGrayColor;
+        _titleLabel.font = [UIFont systemFontOfSize:13];
+        _titleLabel.numberOfLines = 0;
+        [self addSubview:_titleLabel];
+    }
+    return self;
+}
+
+- (void)setModel:(XYInfomationItem *)model{
+    [super setModel:model];
+    
+    self.backgroundColor = UIColor.clearColor;
+    _titleLabel.text = model.title;
+    _titleLabel.font = model.titleFont;
+    _titleLabel.textColor = model.titleColor;
+    [_titleLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.edges.mas_equalTo(model.tipEdgeInsets);
+    }];
+}
+@end
